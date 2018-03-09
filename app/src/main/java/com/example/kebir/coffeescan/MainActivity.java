cea393da5ec34528a223e235d058890bb01462a7
kebircesko <kebircesko@gmail.com>
2018-03-09 20:54:29 +0100

    comit firs

---
+++ app/src/main/java/com/example/kebir/coffeescan/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kebir.coffeescan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
