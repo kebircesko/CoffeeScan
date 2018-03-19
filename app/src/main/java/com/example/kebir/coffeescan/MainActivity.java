57d8161ff6c6b2c9c7f3beef98ae045cebd6a40b
kebircesko <kebircesko@gmail.com>
2018-03-19 17:30:20 +0100

    commit05

--- app/src/main/java/com/example/kebir/coffeescan/MainActivity.java
+++ app/src/main/java/com/example/kebir/coffeescan/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.kebir.coffeescan;
 
+import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
     }
 }
