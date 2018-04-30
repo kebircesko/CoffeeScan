98f39029282cc0b081e7aaa8a20c5f63c2f04dc5
MensurBerisha <mensurberisha1@gmail.com>
2018-04-30 14:31:20 +0200

    Permes kodit ne vazhdim eshte mundesuar navigimi i menuse ku kam perdorur funksionin Bottom navigation ku mundesohet kalimi i fragmenteve nga njera tek tjetra

---
+++ app/src/main/java/com/example/kebir/coffeescan/PijeFragment.java
@@ -0,0 +1,17 @@
+package com.example.kebir.coffeescan;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PijeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pije, container, false);
+    }
+}
