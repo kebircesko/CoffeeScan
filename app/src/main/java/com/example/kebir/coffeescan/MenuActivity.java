f5b1b3737026d21c09575bb4e85623e7a1e7f517
MensurBerisha <mensurberisha1@gmail.com>
2018-04-30 14:26:56 +0200

    Permes kodit ne vazhdim eshte mundesuar navigimi i menuse ku kam perdorur funksionin Bottom navigation ku mundesohet kalimi i fragmenteve nga njera tek tjetra

--- app/src/main/java/com/example/kebir/coffeescan/MenuActivity.java
+++ app/src/main/java/com/example/kebir/coffeescan/MenuActivity.java
@@ -1,7 +1,11 @@
 package com.example.kebir.coffeescan;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new KafeFragment()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new KafeFragment();
+                            break;
+                        case R.id.nav_favorites:
+                            selectedFragment = new PijeFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new UshqimFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
