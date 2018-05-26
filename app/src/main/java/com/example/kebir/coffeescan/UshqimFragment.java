c8a82cc91b50343d9161f57c2fb994d6d8764c91
GentjanQeriqi <gentqeriqi96@gmail.com>
2018-05-26 03:24:05 +0200

    Implementimi i recyclerview ne fragmente

--- app/src/main/java/com/example/kebir/coffeescan/UshqimFragment.java
+++ app/src/main/java/com/example/kebir/coffeescan/UshqimFragment.java
@@ -3,15 +3,32 @@ package com.example.kebir.coffeescan;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+
 
 public class UshqimFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ushqim, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_ushqim, container,false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.ushqimRecycler);
+
+        ListAdapter2 listAdapter2 = new ListAdapter2();
+
+        recyclerView.setAdapter(listAdapter2);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        return view;
     }
 }
