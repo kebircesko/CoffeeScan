d10ad3ddbc8280da3924d45ebf6c0f98de93761d
kebir <kebircesko@gmail.com>
2018-05-29 23:18:06 +0200

    Eshte shtuar 4 fragmente per komunikimin e klientit me serverin ,desktop aplikacioni eshte krijuar me NetBean me java kode (kjo eshte ne trello .rar file),eshte bere dizajnimi i chat-it qe behet mes klientit dhe serverit,por nuk eshte bere funksionale ende klienti te dergoj serverit porosine,ndersa serveri punon me IP-te ,kjo eshte vertetuar me nje projekt tjeter qe ka qene funksionale , por ne projektin ton ende nuk eshte perfunduar.

---
+++ app/src/main/java/com/example/kebir/coffeescan/WelcomePage.java
@@ -0,0 +1,44 @@
+package com.example.kebir.coffeescan;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class WelcomePage extends Activity {
+    private Button send;
+    private EditText ipPrompt;
+    public static String serverIp;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_porosia);
+
+        send = (Button)findViewById(R.id.enterIP);
+        ipPrompt = (EditText) findViewById(R.id.ipPrompt);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                serverIp =  ipPrompt.getText().toString();
+                if ( !serverIp.matches("[" + .0123456789 + "]+") || serverIp.contains(" ") || serverIp.isEmpty() || serverIp.equals(null) || serverIp == null ) {
+                    ipPrompt.setText("");
+                    Toast.makeText(WelcomePage.this, "Invalid IP Address!", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Intent i = new Intent( view.getContext(), MyActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+    }
+
+    public static String getServerIp() {
+        return serverIp;
+    }
+}
